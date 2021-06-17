cfeddc8193ce2de547237780bd8b7314feaa5ed4
knoxie87 <69831595+knoxie87@users.noreply.github.com>
2021-06-17 20:06:58 +1200

    images

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,8 +7,9 @@
 
         <style>
         .card-image .responsive-img {
-        width: 100%;
-        height: 100%;
+        width:100%;
+        height:100%;
+        object-fit: contain;
         }
         </style>
 </head>
